720de3e567f78053d5f23f9d052427b8cab78096
Laura Rodrigues <laurarodriguesalmeida2020@gmail.com>
2025-01-25 16:37:30 -0300

    PDF

--- models/Card.js
+++ models/Card.js
@@ -10,7 +10,9 @@ const CardSchema = new Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'List',
     required: true
-  }
+  },
+  filePath: { type: String, default: null }, // Caminho do arquivo salvo
+  fileName: { type: String, default: null } // Nome do arquivo
 });
 
 const Card = mongoose.models.Card || mongoose.model('Card', CardSchema);
