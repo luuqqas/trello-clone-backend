be40b40d74957cded6284848d5b07b2b5053d520
Laura Rodrigues <laurarodriguesalmeida2020@gmail.com>
2025-01-25 21:26:23 -0300

    Anexar PDFs a cartao (armazenando path para pasta)

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,25 @@ const router = express.Router();
 const Card = require('../models/Card');
 const List = require('../models/List');
 const { authenticateToken } = require('./auth');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadPath = path.join(__dirname, '../uploads');
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage });
 
 router.post('/', authenticateToken, async (req, res) => {
   try {
@@ -75,8 +94,10 @@ router.put('/move/:id', authenticateToken, async (req, res) => {
 
 
 // Adicione esta rota para atualizar o conteúdo do cartão
-router.put('/:id', authenticateToken, async (req, res) => {
+router.put('/:id', authenticateToken,  upload.single('file'), async (req, res) => {
   try {
+    console.log('Arquivo recebido:', req.file); // Log do arquivo recebido
+    console.log('Body:', req.body); // Log do conteúdo enviado no body
     const { id } = req.params;
     const { content } = req.body;
 
@@ -85,14 +106,23 @@ router.put('/:id', authenticateToken, async (req, res) => {
       return res.status(404).json({ error: 'Cartão não encontrado' });
     }
     card.content = content;
+
+    if (req.file) {
+      card.filePath = req.file.path; // Salva o caminho do arquivo
+      card.fileName = req.file.originalname; // Salva o nome original do arquivo
+    }
     await card.save();
 
-    res.status(200).json(card);
+    res.status(200).json({
+      content: card.content,
+      fileName: card.fileName || null // Retorna o nome do arquivo, se houver
+    });
   } catch (error) {
     console.error('Erro ao atualizar cartão:', error);
     res.status(500).json({ error: 'Erro ao atualizar cartão' });
   }
 });
+
 
 router.delete('/:id', authenticateToken, async (req, res) => {
   try {
